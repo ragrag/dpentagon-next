88ced45afc7b97efc4392590940c4976c4cc3906
Raggi <35541698+ragrag@users.noreply.github.com>
2021-01-13 21:19:42 +0200

    home-meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
+import Head from 'next/head';
 import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import AdSection from '../components/AdSection/AdSection';
 import NavBar from '../components/Header/NavBar/NavBar';
 export default function Home() {
   return (
-    <div>
-      <br></br>
-      <NavBar></NavBar>
-      <Row className="justify-content-md-center">
-        <Col className="text-center" md="8">
-          <AdSection />
-        </Col>
-      </Row>
-      <Row className="justify-content-md-center">
-        <Col md="8"></Col>
-      </Row>
-    </div>
+    <>
+      <Head>
+        <title>DPentagon- HOME</title>
+      </Head>
+      <div>
+        <br></br>
+        <NavBar></NavBar>
+        <Row className="justify-content-md-center">
+          <Col className="text-center" md="8">
+            <AdSection />
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center">
+          <Col md="8"></Col>
+        </Row>
+      </div>
+    </>
   );
 }
