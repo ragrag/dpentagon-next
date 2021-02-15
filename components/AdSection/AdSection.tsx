646ba50c1001c6bf8ace46b542f9cf1b2493d003
Raggi <35541698+ragrag@users.noreply.github.com>
2021-02-15 23:04:20 +0200

    ad_section

--- components/AdSection/AdSection.tsx
+++ components/AdSection/AdSection.tsx
@@ -1,25 +1,29 @@
 import Image from 'next/image';
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Modal } from 'react-bootstrap';
 
 export default function AdSection() {
   const [sectionHovered, setSectionHovered] = React.useState(false);
+  const [modalVisible, setModalVisibility] = React.useState(false);
+
   return (
     // <Image width="1024" height="480" src="/ads-space.jpg" alt="Card image"></Image>
-    <Card
-      className="bg-dark text-white"
-      onMouseEnter={() => {
-        setSectionHovered(true);
-      }}
-      onMouseLeave={() => {
-        setSectionHovered(false);
-      }}
-    >
-      <Card.Img as={Image} width="1024" height="480" src="/black-bg.jpg" alt="Card image" />
-      {/* <div style={{ backgroundColor: '#000', width: '1024', height: '480' }}></div> */}
-      <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
-        <Card.Text>
-          {/* {sectionHovered ? (
+    <>
+      <Card
+        className="bg-dark text-white hoverable"
+        onMouseEnter={() => {
+          setSectionHovered(true);
+        }}
+        onMouseLeave={() => {
+          setSectionHovered(false);
+        }}
+        onClick={() => setModalVisibility(true)}
+      >
+        <Card.Img as={Image} width="1440" height="640" src={sectionHovered ? '/ad_cover_hover.jpg' : '/ad_cover.jpg'} alt="Card image" />
+        {/* <div style={{ backgroundColor: '#000', width: '1024', height: '480' }}></div> */}
+        <Card.ImgOverlay className="d-flex justify-content-center align-items-center">
+          <Card.Text>
+            {/* {sectionHovered ? (
             <h2 className="align-self-center mx-auto hoverable" style={{ fontFamily: 'Athelas' }}>
               Read More
             </h2>
@@ -28,8 +32,12 @@ export default function AdSection() {
               Info & Ads
             </h1>
           )} */}
-        </Card.Text>
-      </Card.ImgOverlay>
-    </Card>
+          </Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+      <Modal show={modalVisible} onHide={() => setModalVisibility(false)} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+        <img src="/ad_info.png"></img>
+      </Modal>
+    </>
   );
 }
